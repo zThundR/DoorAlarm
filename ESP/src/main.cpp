22c7a3980c312f33e7120e2d58b316f902199df9
zThundR <95915756+zThundR@users.noreply.github.com>
2022-03-18 12:54:59 +0100

    Solve problem to ESP

--- ESP/src/main.cpp
+++ ESP/src/main.cpp
@@ -6,13 +6,17 @@
 #define echoPin D2
 
 Ultrasonic ultrasonic(trigPin, echoPin);
-long durata, cm;
+long durata, cm, cm2;
+
+const String host = "10.11.137.86";
 
 
 void setup()
 {
     Serial.begin(9600);
-    /*Serial.println();
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    Serial.println();
 
     WiFi.begin("martino", "12345678");
 
@@ -20,25 +24,29 @@ void setup()
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
-        Serial.print(".");
     }
-    Serial.println();
-
-    Serial.print("Connesso, indirizzo IP: ");
-    Serial.println(WiFi.localIP());*/
-
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
 }
 
 void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-    
-    unsigned long time = pulseIn(echoPin, HIGH);
-    cm = 0.03438 * time / 2;
+    durata = pulseIn(echoPin, HIGH);
+    cm = 0.03431 * durata / 2; // per i pollici la formula è durata / 148;
+    Serial.print("Distanza = " + String(cm) + " cm\n");
+    delay(1000);
 
-    Serial.println("Distanza: " + String(cm) + " cm");
-    delay(500);
+
+    WiFiClient client;
+    Serial.println("[Connession a ...]" + String(host));
+
+    if(client.connect (host, 8123)) {
+        Serial.println("Connesso");
+    } else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 5 seconds");
+    }
 }
