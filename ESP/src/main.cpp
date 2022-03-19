e98279cb2e3a7225872d74eef8d0b6d5e08eed34
zThundR <95915756+zThundR@users.noreply.github.com>
2022-03-19 09:28:59 +0100

    Update ESP Code

--- ESP/src/main.cpp
+++ ESP/src/main.cpp
@@ -1,33 +1,40 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <Ultrasonic.h>
 
 #define trigPin D1
 #define echoPin D2
 
 Ultrasonic ultrasonic(trigPin, echoPin);
-long durata, cm, cm2;
+long durata, cm;
+long now = millis();
+long lastMeasure = 0;
 
 const String host = "10.11.137.86";
+const char* ssid = "martino";
+const char* password = "12345678";
 
-
-void setup()
-{
-    Serial.begin(9600);
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
-    Serial.println();
-
-    WiFi.begin("martino", "12345678");
-
-    Serial.print("Tentativo di Connessione");
-    while (WiFi.status() != WL_CONNECTED)
-    {
+void wifi() {
+    delay(10);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
         delay(500);
+        Serial.print(".");
     }
 }
 
+void setup() {
+    Serial.begin(115200);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    wifi();
+}
+
 void loop() {
+    if(!Client)
+    
+    
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
@@ -37,16 +44,4 @@ void loop() {
     cm = 0.03431 * durata / 2; // per i pollici la formula è durata / 148;
     Serial.print("Distanza = " + String(cm) + " cm\n");
     delay(1000);
-
-
-    WiFiClient client;
-    Serial.println("[Connession a ...]" + String(host));
-
-    if(client.connect (host, 8123)) {
-        Serial.println("Connesso");
-    } else {
-        Serial.print("failed, rc=");
-        Serial.print(client.state());
-        Serial.println(" try again in 5 seconds");
-    }
 }
